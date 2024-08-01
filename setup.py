b71f6caead238a54bda79c36098ed5d099af2e99
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-08-01 15:21:29 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='film69',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=[
        "minimalmodbus",
-       "transformers==4.42.3",
+       "transformers==4.43.3",
        "sentence-transformers==3.0.1",
        "llama-index-vector-stores-milvus==0.1.20",
        "llama-index==0.10.52",
