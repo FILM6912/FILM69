3a2d8936b05b4dc5873b7b4d5c134def855f7a19
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-08-05 23:39:41 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,35 +1,29 @@
 from setuptools import setup, find_packages
 
-def parse_requirements(filename):
-    with open(filename) as f:
-        lines = f.readlines()
-    return [line.strip() for line in lines if line.strip() and not line.startswith('#')]
-
 setup(
     name='film69',
     version='0.2.3',
     packages=find_packages(),
     install_requires=[
-    'minimalmodbus',
-    'transformers',
-    'sentence-transformers',
-    'llama-index-vector-stores-milvus',
-    'llama-index==0.10.52',
-    'llama-index-embeddings-huggingface',
-    'pymilvus',
-    'openai',
-    'accelerate',
-    'datasets',
-    'xformers',
-    'trl',
-    'peft',
-    'bitsandbytes'
-],
-    dependency_links=[
-        'git+https://github.com/unslothai/unsloth.git#egg=unsloth',
+        'minimalmodbus',
+        'transformers',
+        'sentence-transformers',
+        'llama-index-vector-stores-milvus',
+        'llama-index==0.10.52',
+        'llama-index-embeddings-huggingface',
+        'pymilvus',
+        'openai',
+        'accelerate',
+        'datasets',
+        'xformers',
+        'trl',
+        'peft',
+        'bitsandbytes',
+        'unsloth @ git+https://github.com/unslothai/unsloth.git#egg=unsloth',
+        # Add additional requirements if needed
     ],
     author='Watcharaphon Pamayayang',
     author_email='filmmagic45@gmail.com',
-    # description='A brief description of the package',
-    # url='URL of the project if available',
+    # description='A brief description of the package',  # Update this with your package's description
+    url='https://github.com/watcharaphon6912',  # Update this with your project's URL
 )
