3a503910703ee976d9a050bc00b4f391b9af0923
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-08-05 23:21:04 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    return [line.strip() for line in lines if line.strip() and not line.startswith('#')]
+
 setup(
     name='film69',
     version='0.2.3',
     packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
     author='Watcharaphon Pamayayang',
     author_email='filmmagic45@gmail.com',
     # description='A brief description of the package',
