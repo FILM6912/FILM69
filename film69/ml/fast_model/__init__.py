a5d4a9eba412c5a10a784386f1c4c8e92a290cd4
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-09-10 14:20:15 +0700

    Update __init__.py

--- film69/ml/fast_model/__init__.py
+++ film69/ml/fast_model/__init__.py
@@ -3,11 +3,16 @@ from unsloth import FastLanguageModel
 import datasets
 import pandas as pd
 from trl import SFTTrainer
-from transformers import TrainingArguments
+from transformers import TrainingArguments,TextIteratorStreamer
+# from transformers import AutoModelForCausalLM, AutoTokenizer,TextIteratorStreamer
 import torch
+from threading import Thread
 from unsloth import is_bfloat16_supported
 
-class FastLLMTrain:
+class FastLLM:
+    def __init__(self):
+        self.history = []
+
     def load_model(self,model_name,dtype=None,load_in_4bit=True,**kwargs):  
         self.model, self.tokenizer = FastLanguageModel.from_pretrained(
             model_name = model_name,
@@ -30,8 +35,11 @@ class FastLLMTrain:
             use_rslora = False,  # We support rank stabilized LoRA
             loftq_config = None, # And LoftQ
         )
+        
 
     def load_dataset(self,df):
+       
+        
         alpaca_prompt = """
         ### Instruction:
         {}
@@ -93,5 +101,53 @@ class FastLLMTrain:
     
     def save_model(self,model_name,save_method = "merged_16bit",**kwargs):
         self.model.save_pretrained_merged(model_name, self.tokenizer, save_method = save_method,**kwargs)
+        
+        
+    def generate_locals(self,text:str,max_new_tokens:int=512,stream:bool=False,history_save:bool=True):
+        FastLanguageModel.for_inference(self.model)
+        if history_save:self.history.append({"role":"user","content":text})
+        self.streamer = TextIteratorStreamer(self.tokenizer, skip_prompt=False, skip_special_tokens=True)
+        input_ids = self.tokenizer.apply_chat_template(self.history if history_save else [{"role": "user","content": text}],add_generation_prompt=True,return_tensors="pt").to(self.model.device)
+        terminators = [self.tokenizer.eos_token_id,self.tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+        if stream==True:
+            thread = Thread(target=self.model.generate, kwargs=
+                            {"input_ids": input_ids,
+                            "streamer": self.streamer,
+                            "max_new_tokens": max_new_tokens,
+                            "eos_token_id":terminators,
+                            "do_sample":True,
+                            "temperature":0.4,
+                            "top_p":0.9,
+                                
+                            })
+            thread.start()
+            
+            def inner():
+                i=0
+                text_out=""
+                if history_save:self.history.append({"role": "system","content": text_out})
+                for new_text in self.streamer:
+                    i+=1
+                    if i!=1:
+                        text_out+=new_text
+                        if history_save:self.history[-1]={"role": "system","content": text_out}
+                        yield  new_text
+
+                thread.join() 
+            return inner()
+        else:
+            outputs = self.model.generate(
+                input_ids,
+                max_new_tokens=max_new_tokens,
+                eos_token_id=terminators,
+                do_sample=True,
+                temperature=0.4,
+                top_p=0.95,
+            )
+            response = outputs[0][input_ids.shape[-1]:]
+            text_out=self.tokenizer.decode(response, skip_special_tokens=True)
+
+            if history_save:self.history.append({"role": "system","content": text_out})
+            return text_out
 
     
