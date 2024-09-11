92293e8fb494e25eddcedcf5a5348c8301a6b5f4
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-09-12 01:25:54 +0700

    Update vectordb.py

--- film69/ml/vectordb.py
+++ film69/ml/vectordb.py
@@ -24,15 +24,16 @@ from chromadb.api.types import (
 )
 
 class CustomEmbeddingFunction(EmbeddingFunction):
-    def __init__(self,embedding_name):
-        self.model = SentenceTransformer(embedding_name)
+    def __init__(self,embedding):
+        self.model = embedding
     def __call__(self, inputs):
         return self.model.encode(inputs).tolist()
 
 class VectorDB:
     def __init__(self,path="database", collection_name="data", embedding_name='sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'):
+        self.embedding_model = SentenceTransformer(embedding_name)
         client = chromadb.PersistentClient(path=path)
-        self.db = client.get_or_create_collection(collection_name,embedding_function=CustomEmbeddingFunction(embedding_name))
+        self.db = client.get_or_create_collection(collection_name,embedding_function=self.embedding_model)
         print("Loaded successfully")
     
     def add_or_update(self, 
