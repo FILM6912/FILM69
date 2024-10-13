efa052bc797cd1b2cb901f50bc13981bc3ab7476
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-10-13 22:08:40 +0700

    Update __init__.py

--- film69/ml/__init__.py
+++ film69/ml/__init__.py
@@ -1,6 +1,6 @@
-from whisper import Whisper
-from whisper import eval as whisper_eval
-from llama import Llama
-from llm_rag import LlmRag_PromptEngineering
-from model import LLMModel
-from vectordb import VectorDB
+from .whisper import Whisper
+from .whisper import eval as whisper_eval
+from .llama import Llama
+from .llm_rag import LlmRag_PromptEngineering
+from .model import LLMModel
+from .vectordb import VectorDB
