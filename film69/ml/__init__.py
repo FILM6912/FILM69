ab0bccf436bd5f1225ca1bff8d2f05fbcc40a785
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-10-15 14:49:13 +0700

    Update __init__.py

--- film69/ml/__init__.py
+++ film69/ml/__init__.py
@@ -2,8 +2,11 @@ from .whisper import Whisper
 from .whisper import eval as whisper_eval
 from .model import LLMModel
 from .vectordb import VectorDB
+
 try:from .fast_model import FastLLM
-except:pass
+except:print("Unable to import FastLLM")
+try:from .llm_rag_chromadb import LlmRagChromadb
+except: print("Unable to import LlmRagChromadb")
 try:from .llama import Llama
-except:pass
+except:print("Unable to import Llama")
 
