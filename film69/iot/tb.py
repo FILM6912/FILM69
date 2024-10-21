793e522d9ad89b000a1b8c55331cacfe979d8f92
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-10-21 17:40:22 +0700

    Update tb.py

--- film69/iot/tb.py
+++ film69/iot/tb.py
@@ -31,7 +31,7 @@ class ThingsBoard:
 
       # print(response.text)
 
-  def get_data(self,para=list[str],limit=24,startTS="",endTS="",device_ID=""):
+  def get_data(self,para=list[str],limit=24,startTS="",endTS="",device_ID="",time=None):
       start=int(datetime.strptime(startTS, "%Y-%m-%d %H:%M:%S").timestamp()*1000)
       end=int(datetime.strptime(endTS, "%Y-%m-%d %H:%M:%S").timestamp()*1000)
       
@@ -50,11 +50,21 @@ class ThingsBoard:
                 formatted_data[key].append(item["value"])
                 ts.append(item["ts"])
         formatted_data["ts"]=ts
+        
+        max_key = min(formatted_data, key=lambda k: len(formatted_data[k]))
+        for i in formatted_data.keys():
+          formatted_data[i]=formatted_data[i][:len(formatted_data[max_key])]
+        
         df=pd.DataFrame(formatted_data)
         df["ts"]=df["ts"].apply(lambda value:datetime.fromtimestamp(value/1000))
         df['ts'] = pd.to_datetime(df['ts'], dayfirst=True)
-        return df.sort_values("ts").reset_index().drop(columns="index")[["ts"]+para]
-
+        df=df.sort_values("ts").reset_index().drop(columns="index")[["ts"]+para]
+        if time!=None:
+          df.set_index('ts', inplace=True)
+          df=df.resample(time).nearest()
+          
+          return df.reset_index()
+        else:return df
       else:return f"Error{response.status_code}"
 
 if __name__ == "__main__":
@@ -64,7 +74,6 @@ if __name__ == "__main__":
   start = (end-timedelta(days=1))
   startTS=start.strftime("%Y-%m-%d %H:%M:%S")
   endTS=end.strftime("%Y-%m-%d %H:%M:%S")
-  
   
   x=tb.get_data(["Humidity","Soil Moisture_1","Soil Moisture_2",
           "Soil Moisture_3","Soil Moisture_4",
