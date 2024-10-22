4071f560c4588034fd961b36ba8cff98c56ff8ca
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-10-22 10:16:06 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="film69",
-    version="0.4.4",
+    version="0.4.5",
     description="",  # Add your package's description here
     author="Watcharaphon Pamayayang",
     author_email="filmmagic45@gmail.com",
