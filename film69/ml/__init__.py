367ae107e1ffab7e692ee25466d0a9887cf5991d
watcharaphon pamayayang <watcharaphon@weaverbase.com>
2024-11-14 16:46:49 +0700

    Update __init__.py

--- film69/ml/__init__.py
+++ film69/ml/__init__.py
@@ -1,9 +1,10 @@
-from .whisper import Whisper
-from .whisper import eval as whisper_eval
 from .model import LLMModel
 from .vectordb import VectorDB
 from .data_classification import DataClassification
-
+try:
+    from .whisper import Whisper
+    from .whisper import eval as whisper_eval
+except:print("Unable to import Whisper")
 try:from .fast_model import FastLLM
 except:print("Unable to import FastLLM")
 try:from .llm_rag_chromadb import LlmRagChromadb
