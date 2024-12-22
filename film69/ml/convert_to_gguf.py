f2626ff452221048bb9edb001f86c7f8170d096b
watcharaphon pamayayang <watcharaphon@weaverbase.com>
2024-12-22 15:44:04 +0700

    Update convert_to_gguf.py

--- film69/ml/convert_to_gguf.py
+++ film69/ml/convert_to_gguf.py
@@ -1,12 +1,15 @@
 import platform,os
 
-def convert_to_gguf(dir_model:str,output_name:str=None,quantization_method:list[str]= ["q8_0","f16"]):
+def convert_to_gguf(dir_model:str,output_name:str=None,quantization_method:list[str]= ["q8_0","f16"],install_req=False):
     if platform.system()=="Linux":re=os.popen("ls").read().split("\n")
     else:re=os.popen("dir /b").read().split("\n")
 
     if not "llama.cpp" in re:
-        os.system("git clone https://github.com/ggerganov/llama.cpp")
-        with open(os.devnull, 'w') as devnull:os.system("cd ./llama.cpp && pip install -r requirements.txt > /dev/null 2>&1")
+        os.system("git clone https://github.com/ggerganov/llama.cpp.git")
+        
+        with open(os.devnull, 'w') as devnull:
+            req='&& pip install -r requirements.txt' if install_req else ""
+            os.system(f"cd ./llama.cpp {req} > /dev/null 2>&1")
     
     for i in quantization_method:
         command = f"python llama.cpp/convert_hf_to_gguf.py {dir_model} --outfile {output_name if output_name !=None else dir_model}.{i.upper()}.gguf --outtype {i}"
