bf0c43c7dbb97be2c93d6ac1b6a9ed00b0add376
watcharaphon pamayayang <watcharaphon@weaverbase.com>
2025-01-04 17:37:37 +0700

    Update lang_chain_fast_llm.py

--- FILM69/ml/langchain/lang_chain_fast_llm.py
+++ FILM69/ml/langchain/lang_chain_fast_llm.py
@@ -1,59 +1,99 @@
-from typing import Any, Dict, Iterator, List, Mapping, Optional
+from typing import Any, Dict, Iterator, List, Optional
 
-from langchain_core.callbacks.manager import CallbackManagerForLLMRun
-from langchain_core.language_models.llms import LLM
-from langchain_core.outputs import GenerationChunk
-from ..fast_model import FastLLM
+from langchain_core.callbacks import (
+    CallbackManagerForLLMRun,
+)
+from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import (
+    AIMessage,
+    AIMessageChunk,
+    BaseMessage,
+)
+from langchain_core.messages.ai import UsageMetadata
+from langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
+from pydantic import Field
+from FILM69.ml import FastLLM
 
-class LangChainFastLLM(LLM):
-    # model_name:str
-    model_llm: FastLLM=None
-    # top_k:int=15
-    # top_p:float=0.95
-    # temperature:float=0.2
+class LangChainFastLLM(BaseChatModel):
+    model_name: str
+    model_llm:FastLLM=None
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model_llm=FastLLM()
+        del kwargs["model_name"]
         self.model_llm.load_model(
+            model_name=self.model_name,
             **kwargs
             )
-    
-    def _call(
+
+    def _generate(
         self,
-        prompt: str,
+        messages: List[BaseMessage],
         stop: Optional[List[str]] = None,
-        max_new_tokens=512,
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
-    ) -> str:
+    ) -> ChatResult:
+        
+        tokens = self.model_llm.generate(messages)
+        ct_input_tokens = sum(len(message.content) for message in messages)
+        ct_output_tokens = len(tokens)
+        message = AIMessage(
+            content=tokens,
+            additional_kwargs={},
+            response_metadata={
+                "time_in_seconds": 3,
+            },
+            usage_metadata={
+                "input_tokens": ct_input_tokens,
+                "output_tokens": ct_output_tokens,
+                "total_tokens": ct_input_tokens + ct_output_tokens,
+            },
+        )
 
-        if stop is not None:
-            raise ValueError("stop kwargs are not permitted.")
-        
-        
-        return self.model_llm.generate(prompt,history_save=False,max_new_tokens=max_new_tokens)
+        generation = ChatGeneration(message=message)
+        return ChatResult(generations=[generation])
 
     def _stream(
         self,
-        prompt: str,
+        messages: List[BaseMessage],
         stop: Optional[List[str]] = None,
-        max_new_tokens=512,
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
-    ) -> Iterator[GenerationChunk]:
-        for char in self.model_llm.generate(prompt,history_save=False,max_new_tokens=max_new_tokens,stream=True):
-            chunk = GenerationChunk(text=char)
+    ) -> Iterator[ChatGenerationChunk]:
+        ct_input_tokens = sum(len(message.content) for message in messages)
+
+        for token in self.model_llm.generate(messages,stream=True):
+            usage_metadata = UsageMetadata(
+                {
+                    "input_tokens": ct_input_tokens,
+                    "output_tokens": 1,
+                    "total_tokens": ct_input_tokens + 1,
+                }
+            )
+            ct_input_tokens = 0
+            chunk = ChatGenerationChunk(
+                message=AIMessageChunk(content=token, usage_metadata=usage_metadata)
+            )
+
             if run_manager:
-                run_manager.on_llm_new_token(chunk.text, chunk=chunk)
+                run_manager.on_llm_new_token(token, chunk=chunk)
 
             yield chunk
+
+        chunk = ChatGenerationChunk(
+            message=AIMessageChunk(content="", response_metadata={"time_in_sec": 3})
+        )
+        if run_manager:
+            run_manager.on_llm_new_token(token, chunk=chunk)
+        yield chunk
+
+    @property
+    def _llm_type(self) -> str:
+        return "echoing-chat-model-advanced"
 
     @property
     def _identifying_params(self) -> Dict[str, Any]:
         return {
-            "model_name": "LangChainFastLLM",
+            "model_name": self.model_name,
         }
-
-    @property
-    def _llm_type(self) -> str:
-        return "lang_chain_fast_llm"
