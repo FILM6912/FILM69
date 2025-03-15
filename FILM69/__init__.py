03ae3e180cdfcdb40732b20801bbb855a4e1ce71
watcharaphon pamayayang <watcharaphon@weaverbase.com>
2025-03-16 01:05:33 +0700

    Update __init__.py

--- FILM69/__init__.py
+++ FILM69/__init__.py
@@ -1,7 +1,9 @@
 import warnings
 warnings.simplefilter("ignore", UserWarning)
+from .dis_print import dis_print
 
-try:from .ml import *
-except:...
-from .datasets.clean_text import clean_text
-from .tts import TTS
+with dis_print():
+    try:from .ml import *
+    except:...
+    from .datasets.clean_text import clean_text
+    from .tts import TTS
