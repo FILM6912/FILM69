4decc4d6e750e1bd57b6122fe028e147179eee14
watcharaphon pamayayang <watcharaphon@weaverbase.com>
2025-03-29 14:11:46 +0700

    delete

--- FILM69/ml/fast_model/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys,os
-from ...DisPrint import dis_print
-with dis_print():
-    from .fast_llm import FastLLM
-    from .fast_vision import FastVLLM
-    from .auto_model import FastAutoModel
