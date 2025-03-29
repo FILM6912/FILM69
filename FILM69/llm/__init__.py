837586748773dc938ff6e3085be6a0651120d9cb
watcharaphon pamayayang <watcharaphon@weaverbase.com>
2025-03-29 14:10:22 +0700

    Create __init__.py

---
+++ FILM69/llm/__init__.py
@@ -0,0 +1,16 @@
+from ..DisPrint import dis_print
+with dis_print():
+    from .model import LLMModel
+    try:from .vectordb import VectorDB
+    except:print("Unable to import VectorDB")
+    try:from .fast_model import *
+    except:print("Unable to import FastModel")
+    try:from .llm_rag_chromadb import LlmRagChromadb
+    except: print("Unable to import LlmRagChromadb")
+    try:from .llama import Llama
+    except:pass
+    
+    from .convert_to_gguf import convert_to_gguf
+    from .langchain import LangChainFastLLM
+    from .requires_memory import requires_memory
+
