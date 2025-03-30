d00d50f414f085e6563bc4ee75af53582abbfc45
watcharaphon pamayayang <watcharaphon@weaverbase.com>
2025-03-30 12:45:40 +0700

    Update __init__.py

--- FILM69/__init__.py
+++ FILM69/__init__.py
@@ -1,10 +1,14 @@
 def torch_parameters_model(model):
     num = sum(p.numel() for p in model.parameters())
     if num >= 1e9:
-        return f"{num / 1e9:.3f}B"
+        total_parameters= f"{num / 1e9:.3f}B"
     elif num >= 1e6:
-        return f"{num / 1e6:.3f}M"
+        total_parameters= f"{num / 1e6:.3f}M"
     elif num >= 1e3:
-        return f"{num / 1e3:.3f}K"
+        total_parameters= f"{num / 1e3:.3f}K"
     else:
-        return str(num)
+        total_parameters= str(num)
+    
+    total_layers = sum(1 for _ in model.children())
+
+    return total_parameters,total_layers
