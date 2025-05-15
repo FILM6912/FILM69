7473773df5d95c7bc740e34bc938a251f758c2f3
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2025-05-16 01:45:16 +0700

    Update convert_to_gguf.py

--- FILM69/llm/convert_to_gguf.py
+++ FILM69/llm/convert_to_gguf.py
@@ -1,6 +1,6 @@
 import platform,os
 
-def convert_to_gguf(dir_model:str,output_name:str=None,quantization_method:list[str]= ["q8_0","f16"],install_req=False):
+def convert_to_gguf(dir_model:str,output_name:str=None,quantization_method:list[str]= ["q4_0","q8_0","f16"],install_req=False):
     if platform.system()=="Linux":re=os.popen("ls").read().split("\n")
     else:re=os.popen("dir /b").read().split("\n")
 
@@ -8,12 +8,22 @@ def convert_to_gguf(dir_model:str,output_name:str=None,quantization_method:list[
         os.system("git clone https://github.com/ggerganov/llama.cpp.git")
         
         with open(os.devnull, 'w') as devnull:
+            print("Installing llama.cpp...")
+            
             req='&& pip install -r requirements.txt' if install_req else ""
             os.system(f"cd ./llama.cpp {req} > /dev/null 2>&1")
+            os.system(f"cd llama.cpp && cmake -B build && cmake --build build --config Release -j 8 > /dev/null 2>&1")
+            
+            print("Done!")    
+    
+    command = f"python llama.cpp/convert_hf_to_gguf.py {dir_model} --outfile {output_name if output_name !=None else dir_model}.F16.gguf"
+    os.system(command)
     
     for i in quantization_method:
-        command = f"python llama.cpp/convert_hf_to_gguf.py {dir_model} --outfile {output_name if output_name !=None else dir_model}.{i.upper()}.gguf --outtype {i}"
-        os.system(command)
+        if i != "f16":
+            command = f'llama.cpp/build/bin/llama-quantize {output_name if output_name !=None else dir_model}.F16.gguf {output_name if output_name !=None else dir_model}.{i.upper()}.gguf {i.upper()}'
+            os.system(command)
+            
 
 if __name__=="__main__":
     convert_to_gguf("dir_model","file_name",["q8_0"])
