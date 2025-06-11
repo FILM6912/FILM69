978cb5b4db353a8659003ff27ed20a31bf3b918a
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2025-06-11 16:40:24 +0700

    Update __init__.py

--- FILM69/tools/__init__.py
+++ FILM69/tools/__init__.py
@@ -2,20 +2,28 @@ from .pydantic_from_json import pydantic_from_json
 from .DisPrint import dis_print
 
 def torch_parameters_model(model):
-    num = sum(p.numel() for p in model.parameters())
-    if num >= 1e9:
-        total_parameters= f"{num / 1e9:.3f}B"
-    elif num >= 1e6:
-        total_parameters= f"{num / 1e6:.3f}M"
-    elif num >= 1e3:
-        total_parameters= f"{num / 1e3:.3f}K"
-    else:
-        total_parameters= str(num)
+    total_params = sum(p.numel() for p in model.parameters())
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    
+    def format_number(num):
+        if num >= 1_000_000_000:  # Billion
+            return f"{num / 1_000_000_000:.1f}B"
+        elif num >= 1_000_000:   # Million
+            return f"{num / 1_000_000:.1f}M"
+        elif num >= 1_000:       # Thousand
+            return f"{num / 1_000:.1f}K"
+        else:
+            return str(num)
     
     total_layers = sum(1 for _ in model.children())
-
-    return total_parameters,total_layers
-
+    
+    return {
+        'total': format_number(total_params),
+        "total_layers":total_layers,
+        'trainable': format_number(trainable_params),
+        'total_raw': total_params,
+        'trainable_raw': trainable_params
+    }
 
 def run_index(step,len_files):
     index=0
@@ -25,7 +33,7 @@ def run_index(step,len_files):
         index+=step
         run_index.append([lass,len_files if index>=len_files else index])
     
-    return run_index[:-1]
+    return run_index
 
 
 __all__ = [
